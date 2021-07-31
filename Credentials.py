7a0405d4beb900a60af439d9e0243f8335f079af
benjamin muchemi <benjamin.muchemi@student.moringaschool.com>
2021-07-31 11:36:04 +0300

    import user class and create new short codes.

---
+++ Credentials.py
@@ -0,0 +1,12 @@
+import random
+from users import User
+
+
+def create():
+
+ while True:
+    print("Welcome to PassLOCKER!")
+    print("/n")
+    print("Please user one of the short-cods below to navigate the platform.")
+    print("/n")
+    print("cr to create a new account.2. lg to log into your account")
