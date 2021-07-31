abee244cf82b10e2e0cbd1adf13784522e5e9e3d
benjamin muchemi <benjamin.muchemi@student.moringaschool.com>
2021-07-31 12:12:04 +0300

    rename credentials to run.py

--- run.py
+++ run.py
@@ -1,12 +1,16 @@
 import random
-from users import User
+from Users import User
 
 
-def create():
+def main():
 
  while True:
+    input("me")
     print("Welcome to PassLOCKER!")
     print("/n")
     print("Please user one of the short-cods below to navigate the platform.")
     print("/n")
     print("1.cr to create a new account. 2.lg to log into your account. 3.vi to view your saved passwords. 3.de to delete your saved passwords. 4.ex to exit the platform")
+ 
+ if __name__ == "__main__":
+     main()
