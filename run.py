6fc0f45dd5cd25ac4c98eb0595f719208025d027
benjamin muchemi <benjamin.muchemi@student.moringaschool.com>
2021-07-31 12:53:51 +0300

    validate new user credentials

--- run.py
+++ run.py
@@ -1,16 +1,37 @@
 import random
-from Users import User
-
+from users import User
+ 
 
 def main():
+ print("Welcome to PassLOCKER!")
+ print('\n')
 
- while True:
-    input("me")
-    print("Welcome to PassLOCKER!")
-    print("/n")
-    print("Please user one of the short-cods below to navigate the platform.")
-    print("/n")
-    print("1.cr to create a new account. 2.lg to log into your account. 3.vi to view your saved passwords. 3.de to delete your saved passwords. 4.ex to exit the platform")
- 
- if __name__ == "__main__":
-     main()
+while True:
+   print("Please user one of the short-cods below to navigate the platform.")
+   print('\n')
+   print("1.cr to create a new account. 2.lg to log into your account. 3.vi to view your saved passwords. 3.de to delete your saved passwords. 4.ex to exit the platform")
+   codes = input().lower()
+
+   if codes == cr:
+      print("Create new username")
+      new_username = input("Username..")
+
+      print("Create new password")
+      new_password = input("Password..")
+
+      print("Confirm password")
+      new_confirm = input("Password.")
+
+      while new_confirm != new_password:
+         print("Passwords did not match.Please input correct password")
+         new_password = input("Password..")
+
+         print("Confirm password")
+         new_confirm = input("Password.")
+
+      else:
+         print("Successful")
+         print("Please log in to your account to edit passwords.")
+
+   
+   
