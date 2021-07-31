67f9842c9c3330b729b448d74c4a661876101c5d
benjamin muchemi <benjamin.muchemi@student.moringaschool.com>
2021-07-31 11:28:51 +0300

    created new instance of  a user

---
+++ Users.py
@@ -0,0 +1,27 @@
+class User : 
+
+    """
+    Create new instance of users
+    """
+
+    users_list = []
+
+
+def __init__(self,init_username,init_password):
+
+    self.init_username = init_username
+    self.init_password = init_password
+
+
+def save_users(self):
+
+    """
+    Method to save users and their password to the user_list.
+    """
+    
+    User.users_list.append(self)
+
+
+  
+
+
