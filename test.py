53738e61fb408b74562ac6a8dfaf56bf1e8564cd
benjamin muchemi <benjamin.muchemi@student.moringaschool.com>
2021-08-02 08:58:56 +0300

    create new test file

--- test.py
+++ test.py
@@ -2,15 +2,15 @@ import unittest
 from users import User
 from users import Credentials
 
-class LockerTest(unittest.TestCase):
+class UserTest(unittest.TestCase):
     def setUp(self):
         """
         method run before each test
         """
-        self.new_account = Credentials("instagram","linda","myPassword")
+        self.new_account = Credentials("twitter","Benjamin","1234")
     
     def tearDown(self):
         '''
         method called after each user test
         '''
-        User.data_user = []
+        User.users_list= []
