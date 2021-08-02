5d04a5df6c876b3bf756a6a485b3f1c91a547ccb
benjamin muchemi <benjamin.muchemi@student.moringaschool.com>
2021-08-02 19:54:41 +0300

    create test to test deletion of an ccount.

--- test.py
+++ test.py
@@ -3,12 +3,35 @@ from users import User
 
 class UserTest(unittest.TestCase):
 
-    def setUp(self):
-       self.new_user=User("Benjamin","pass")
+ def setUp(self):
+        '''
+        method run before each user test
+        '''
+        self.new_user = User("Mary", "1234")
 
-    def test_init(self):
-        self.assertEqual(self.new_user.init_username,"James")
-        self.assertEqual(self.new_user.init_password,"1234")
+def tearDown(self):
+        '''
+        method called after each user test
+        '''
+        User.users_list = []
 
-if __name__ == '__main__':
-    unittest.main()
+def test_init(self):
+        '''
+        test method to check if user class is initialize
+        '''
+        self.assertEqual(self.new_user.init_username,"Mary")
+        self.assertEqual(self.new_user.init_password, "1234")
+        
+def test_save_user(self):
+        '''
+        test method to test if user has been saved
+        '''
+        self.new_user.save_users()
+        self.assertEqual(len(User.users_list),1)
+
+def test_user(self):
+        '''
+        test method to test if user has been save
+        '''
+        self.new_user.save_users()
+        self.assertEqual(len(User.users_list),1)
